bd68848b0463e23557f6696a8cbcab54969a4dbe
RAU-HAM <hrayer.rabota@gmail.com>
2015-12-16 20:08:36 +0400

    Update Battlеship.cpp

--- Battlеship.cpp
+++ Battlеship.cpp
@@ -4,39 +4,40 @@
 #include<string>
 #include<map>
 using namespace std;
-const int N = 10; //vor amen angam 10 chgrenq
+const int N = 10;
 const char empty_field = '0'; 
 const char occupied_field = '1';
-const char destroyed_field = '2'; // ete uzenanq poxenq nshanakumnere, es const charerov hesht klni
+const char destroyed_field = '2';
 class Board
 {
 private:
-	char board[N][N]; //matrican, vortex tvyal Board-e pahelu a informacian, 10x10 chapi
+	char board[N][N];
 public:
-	Board(); //konstruktor, default-nerov klni sax empty_field
-	char* operator[](int i); // operator [] vor ete unenanq Board a; karenanq grenq a[i][j]
-	void print(); //tpuma matrican
+	Board(); 
+	char* operator[](int i); 
+	void print(); 
 	bool isFree(int x, int y); //stuguma navi mas karelia @tex dnel te che, stugelov ira bolor 8 koxmere (diagonalnern el hete)
 };
 
-class CommandHandler //ham xaxacoxe ham kompe kunena es klassic, stex linelu en funckianere, 
+class CommandHandler // Игровые фyнкции для игрока и компьютера 
 {
 private:
-	Board& board; //uni Board-i ssilka, vortex pahelu a tvyal xaxacoxi boarde;
-	Board enemyBoard; // mi hat el board inqna stexcum, vor meje pahi ira unecac informacian hakarakordi boardi masin
+	Board& board;
+	Board enemyBoard;
 public:
 	CommandHandler(Board& b) :
-		board(b) {} // qani vor voch initializacvac ssilka chi kareli toxel classi mej, 
-					//senc petqa grvi konstruktorum, vore kstana boarde u miangamic klasse sarqelu jamanak initializacni
-	void Player_fill(); //boardi mej (klassum` Board& board) meje lcnuma navere, xaxacoxi hamar, cinnerov
-	void AI_fill(); // nuynpes lcnuma meje, funkcian kompi lcnelu hamara, randomnerov
+ // qani vor voch initializacvac ssilka chi kareli toxel classi mej, 
+ // senc petqa grvi konstruktorum, vore kstana boarde u miangamic klasse sarqelu jamanak initializacni
+	board(b) {};				
+	void Player_fill();
+	void AI_fill(); 
 };
 
 int main()
 {
-	Board Player_Board, AI_Board; //ham kompe ham xaxacoxe kunena iranc sepakan Board-ere
-	CommandHandler Player(Player_Board), AI(AI_Board); //u iranc sepakan CommandHandlernere
-	Player.Player_fill(); // funkcianeri kanchi dzeve
-	cout << "Finished filling\n";
+	Board Player_Board, AI_Board;
+	CommandHandler Player(Player_Board), AI(AI_Board); //iranc sepakan CommandHandlernere
+	Player.Player_fill();
+	cout << "Finished filling" << endl;
 	return 0;
 }

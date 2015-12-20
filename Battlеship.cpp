d9beed346e47c58005d318971cccc3af58b746c0
RAU-HAM <hrayer.rabota@gmail.com>
2015-12-21 00:59:08 +0300

    Update Battlеship.cpp

--- Battlеship.cpp
+++ Battlеship.cpp
@@ -15,7 +15,48 @@ private:
 public:
 	Board(); 
 	char* operator[](int i); 
-	void print(); 
+	void print()
+	{
+		cout << "    ";
+		for (int c = 0; c < N; c++)
+		{
+			cout << c << " ";
+		}
+		cout << "\n\n";
+		for (int i = 0; i < N; i++)
+		{
+			cout << i << "   ";
+			for (int j = 0; j < N; j++)
+			{
+				cout << board[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	bool isFree(int x, int y)
+	{
+		if (x < 0 || x >= 10 || y < 0 || y >= 10)
+		{
+			return false;
+		}
+		int x1, y1, a[3] = { 0, 1, -1 };
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				x1 = x + a[i];
+				y1 = y + a[j];
+				if (x1 >= 0 && x1 < 10 && y1 >= 0 && y1 < 10)
+				{
+					if (board[x1][y1] != empty_field)
+					{
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
 	bool isFree(int x, int y); //stuguma navi mas karelia @tex dnel te che, stugelov ira bolor 8 koxmere (diagonalnern el hete)
 };
 
@@ -25,6 +66,66 @@ private:
 	Board& board;
 	Board infoBoard;
 	Board enemyBoard;
+        bool try_to_place (int x0, int y0, int x1, int y1, int size, ostream& out) 
+	{
+		while (true) {
+			if (x0 > 9 || y0 > 9 || x1 > 9 || y1 > 9)
+			{
+				out << "\nTry to place your ship INSIDE the board";
+				return false;
+			}
+			if (x0 == x1)
+			{
+				if (y1 - y0 != size - 1)
+				{
+					out << "\nThis should be a ";
+					out << size;
+					out << "-decker ship, try again";
+					return false;
+				}
+				for (int j = y0; j <= y1; j++)
+				{
+					if (!board.isFree(x0, j))
+					{
+						out << "\nKeep distance betwwen the ships!";
+						return false;
+					}
+				}
+				for (int j = y0; j <= y1; j++)
+				{
+					board[x0][j] = occupied_field;
+				}
+			}
+			else if (y0 == y1)
+			{
+				if (x1 - x0 != size - 1)
+				{
+					out << "\nThis should be a ";
+					out << size;
+					out << "-decker ship, try again";
+					return false;
+				}
+				for (int i = x0; i <= x1; i++)
+				{
+					if (!board.isFree(i, y0))
+					{
+						out << "\nKeep distance betwwen the ships!";
+						return false;
+					}
+				}
+				for (int i = x0; i <= x1; i++)
+				{
+					board[i][y0] = occupied_field;
+				}
+			}
+			else
+			{
+				out << "\nYour ship has to be straight, try again";
+				return false;
+			}
+			return true;
+		}
+	}
 public:
      // qani vor voch initializacvac ssilka chi kareli toxel classi mej, 
      // senc petqa grvi konstruktorum, vore kstana boarde u miangamic klasse sarqelu jamanak initializacni
@@ -113,8 +214,32 @@ public:
 			cout << "Incorrect coordinates" << endl;
 		}
 	}
+        void AI_fill()
+	{
+		int x0, y0, x1, y1;
+		auto place = [&](int size) {
+			board.print();
+			do
+			{
+				x0 = rand() % (N - size);
+				y0 = rand() % (N - size);
+				if (rand() % 2 == 0)
+				{
+					x1 = x0;
+					y1 = y0 + size - 1;
+				}
+				else
+				{
+					y1 = y0;
+					x1 = x0 + size - 1;
+				}
+			} while (try_to_place(x0, y0, x1, y1, size, cout) == false);
+		};
+		place(4);
+		for (int i = 3; i > 0; i--)
+			for (int k = 0; k < 5 - i; k++) 
+				place(i);
 	}
-	void AI_fill(); 
 };
 
 int main()
